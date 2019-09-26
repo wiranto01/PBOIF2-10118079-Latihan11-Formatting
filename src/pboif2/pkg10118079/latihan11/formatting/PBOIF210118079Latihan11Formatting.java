2897dbe603f53bfe1024d10b31486200d7d1c519
wiranto01 <55836685+wiranto01@users.noreply.github.com>
2019-09-26 18:28:45 +0700

    Add files via upload

---
+++ src/pboif2/pkg10118079/latihan11/formatting/PBOIF210118079Latihan11Formatting.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10118079.latihan11.formatting;
+
+/**
+ *
+ * @author
+ * Nama : Wiranto
+ * Kelas : If2
+ * Nim : 10118079
+ */
+public class PBOIF210118079Latihan11Formatting {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int i=3546764;
+        int IMinus= -i;
+        System.out.println("i : "+i);
+        System.out.printf("%%d : %d %n", i);
+        System.out.printf("%%10d: %+10d%n", i);
+        System.out.printf("%%+10d : %+10d%n",IMinus);
+        System.out.printf("%%,10d : %,10d%n",i);
+        System.out.printf("%%,10d : %,-10d%n",i);
+        
+        
+        double f=5675482.982;
+        System.out.println("f : "+f);
+        System.out.printf("%%f : %f%n",f);
+        System.out.printf("%%.2f : %.2f%n",f);
+        System.out.printf("%%12.2f : %12.2f%n",f);
+    }
+    
+}
